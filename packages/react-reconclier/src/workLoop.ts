a2d83cb1b68a76c3cbb1c456e8b50773c464886d
陈泽溪 <czx@chenzexideMacBook-Pro.local>
2023-04-30 18:00:14 +0800

    feat: react-reconciler包初步完成

--- packages/react-reconclier/src/workLoop.ts
+++ packages/react-reconclier/src/workLoop.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-constant-condition */
 import { beginWork } from './beginWork'
+import { commitMutationEffects } from './commitWork'
 import { completeWork } from './completeWork'
 import { createWorkInProgress, FiberNode, FiberRootNode } from './fiber'
+import { MutationMask, NoFlags } from './fiberFlags'
 import { HostRoot } from './workTags'
 
 let workInProgress: FiberNode | null = null
@@ -47,6 +50,37 @@ function renderRoot(root: FiberRootNode) {
 	root.finishedWork = finishedWork
 
 	commitRoot(root)
+}
+
+function commitRoot(root: FiberRootNode) {
+	const finishedWork = root.finishedWork
+
+	if (finishedWork === null) {
+		return
+	}
+
+	if (__DEV__) {
+		console.warn('commit阶段开始', finishedWork)
+	}
+
+	// 已经被保存在finishedWork中
+	root.finishedWork = null
+
+	// 判断是否存在3个子阶段需要执行的操作
+	// root flags root subtreeFlags
+	const subtreeHasEffect = (finishedWork.subtreeFlag & MutationMask) !== NoFlags
+
+	const rootHasEffect = (finishedWork.flags & MutationMask) !== NoFlags
+
+	if (subtreeHasEffect || rootHasEffect) {
+		// beforeMutation
+		// Mutation
+		commitMutationEffects(finishedWork)
+		root.current = finishedWork
+		// layout
+	} else {
+		root.current = finishedWork
+	}
 }
 
 function workLoop() {
